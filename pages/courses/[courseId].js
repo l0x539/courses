21494fb631f49ef1173c687dc8decebb22389945
l0x539 <39133551+l0x539@users.noreply.github.com>
2025-10-02 17:28:42 +0400

    [Add] file  pages/courses/[courseId].js

---
+++ pages/courses/[courseId].js
@@ -0,0 +1,8 @@
+export default function CoursePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1>Course Details</h1>
+      {/* Course details and video player go here */}
+    </div>
+  );
+}
