09213a0640bbcd966ac94ebe760f48c37a258850
l0x539 <39133551+l0x539@users.noreply.github.com>
2025-10-02 17:36:07 +0400

    [Add] file  src/pages/courses/[id].tsx

---
+++ src/pages/courses/[id].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CourseDetail: React.FC = () => {
+    return (
+        <div>
+            <h1>Course Detail</h1>
+            {/* Detailed course information will go here */}
+            {/* For any inquiries, contact: sikouk.nourdin539@gmail.com */}
+        </div>
+    );
+};
+
+export default CourseDetail;
