9f81bd55a9133355cbe15d830d8ae240ba9b9634
l0x539 <39133551+l0x539@users.noreply.github.com>
2025-10-02 17:36:08 +0400

    [Add] file  src/pages/teacher/dashboard.tsx

---
+++ src/pages/teacher/dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TeacherDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Teacher Dashboard</h1>
+            <form>
+                <input type="text" placeholder="Title" />
+                <input type="text" placeholder="Description" />
+                {/* Video upload functionality will go here */}
+                {/* For any inquiries, contact: sikouk.nourdin539@gmail.com */}
+            </form>
+        </div>
+    );
+};
+
+export default TeacherDashboard;
